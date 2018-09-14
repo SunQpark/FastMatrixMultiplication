c4747f9134f30a58f78cc52c3832d880206d3c98
SunQ <sunq0313@gmail.com>
2018-09-15 06:20:47 +0900

    initial commit

---
+++ matrix.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include<iostream>
+#include<vector>
+
+
+using scalar = double;
+
+template<class T>
+class Matrix: public vector<T>{
+    vector<unsigned int> shape;
+
+    void Matrix(const vector<T> value){
+        
+    }
+
+
+};
